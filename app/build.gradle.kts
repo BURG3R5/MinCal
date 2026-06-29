76652f12afac9120f3c844915117179b107958a9
Aditya Rajput <adiraj20072002@gmail.com>
2026-06-29 14:28:32 +0530

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "co.adityarajput.mincal"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
